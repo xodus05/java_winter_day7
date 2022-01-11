935f390c7b701824e6de84d1867f599824f92359
Taeyeon <xodus05>
2022-01-11 13:26:19 +0900

    final 필드 값 변경 금지

--- src/test/fianl1/Child.java
+++ src/test/fianl1/Child.java
@@ -1,7 +1,11 @@
 package test.fianl1;
 
 public class Child extends Parent{
+// final 필드는 나중에 값 변경 금지(final 필드 값 변경 금지)
+	public final int time = 3;
+	
 	public void rest() {
-		System.out.println("2시간 휴식한다.");
+//		time = 5;
+		System.out.println(time + "시간 휴식한다.");
 	}
 }
